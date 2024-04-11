4c21c0758fa01e822bdcaf02a9a59d05d693c1e0
Jardel <jardelgalvao1@gmail.com>
2024-04-10 21:34:05 -0300

    Initial commit

---
+++ buy-and-sell/src/app/listing-data-form/listing-data-form.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit, Input, Output, EventEmitter, input } from '@angular/core';
+import { Router } from '@angular/router';
+import { Listing } from '../types'; 
+
+@Component({
+  selector: 'app-listing-data-form',
+  templateUrl: './listing-data-form.component.html',
+  styleUrl: './listing-data-form.component.css'
+})
+export class ListingDataFormComponent {
+  @Input() buttonText: string = '';
+  @Input() currentName: string = '';
+  @Input() currentDescription: string = '';
+  @Input() currentPrice: number = 0;
+
+  name: string = '';
+  description: string = '';
+  price: number = 0;
+
+  @Output() onSubmit = new EventEmitter<Listing>();
+
+  constructor(
+    private router: Router,
+  ){}
+
+  ngOnInit(): void{
+    this.name = this.currentName;
+    this.description = this.currentDescription;
+    this.price = this.currentPrice;
+  }
+
+  onButtonClicked(): void {
+    this.onSubmit.emit({
+      id: '',
+      name: this.name,
+      description: this.description,
+      price: Number(this.price),
+    });
+  }
+}
---
+++ buy-and-sell/src/app/listing-detail-page/listing-detail-page.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { fakeListings } from '../fake-data';
+import { Listing } from '../types';
+
+@Component({
+  selector: 'app-listing-detail-page',
+  templateUrl: './listing-detail-page.component.html',
+  styleUrl: './listing-detail-page.component.css'
+})
+export class ListingDetailPageComponent {
+  listing!: Listing | undefined;
+
+  constructor(
+    private route: ActivatedRoute,
+  ){
+    this.listing = {
+      id: '',
+      name: '',
+      description: '',
+      price: 0,
+    }
+  }
+
+  ngOnInit():void{
+    const id = this.route.snapshot.paramMap.get('id');
+    this.listing = fakeListings.find(listing => listing.id === id);
+  }
+}
